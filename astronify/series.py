72da67f4e7ab893068b0b8d54ebed1cf90f7e0bc
Scott Fleming <fleming@stsci.edu>
2020-04-24 11:17:16 -0400

    exptime in sonify() is now derived directly from the time column instead of hard-coded within the function; cleaned up some minor formatting to make pylint happier

--- astronify/series.py
+++ astronify/series.py
@@ -31,29 +31,30 @@ class SoniSeries():
         self.data = data
         self.sonification_method = method
         self.time_col = time_col
-        self.val_col=val_col
+        self.val_col = val_col
 
         self.pitch_map = data_to_pitch
 
-        if method =="pyo":
+        if method == "pyo":
             self._init_pyo()
-            
 
     def _init_pyo(self):
         self.server = pyo.Server()
         self.streams = None
-        
+
     @property
     def sonification_method(self):
         return self._sonification_method
 
     @sonification_method.setter
     def sonification_method(self, value):
-        assert value.lower() in ("pyo","thinkdsp"), 'The only valid sonification methods are pyo and thinkdsp.'
+        method_warn_str = ('The only valid sonification methods are pyo and'
+                           ' thinkdsp.')
+        assert value.lower() in ("pyo", "thinkdsp"), method_warn_str
         self._sonification_method = value
         if value == "pyo":
             self._init_pyo()
-   
+
     @property
     def data(self):
         return self._data
@@ -89,25 +90,28 @@ class SoniSeries():
     def pitch_map(self, value):
         # TODO: test for valid function here
         self._pitch_map = value
-        
+
     def sonify(self):
         """
-        TODO: document, also ad arguments
+        TODO: document, also add arguments
         """
-
-        exptime = 0.01881188407271875 # LC cadence in days
         duration = 0.5 # note duration in seconds
         spacing = 0.01 # spacing between notes in seconds
         data = self.data
-        
+
+        # NOTE: This assumes the times in the time_col are an astropy Time object
+        # and thus the np.diff results in a TimeDelta object with structured tags
+        # If passing, e.g., simulated data from the simulator, it may not be?
+        exptime = np.median(np.diff(data[self.time_col])).value
+
         data.meta["exposure_time"] = exptime
         data.meta["note_duration"] = duration
         data.meta["spacing"] = spacing
-        
+
         data["pitch"] = self.pitch_map(data[self.val_col])
-        data["onsets"] = [x for x in (data["time"].jd - data["time"].jd[0])/exptime*spacing]
-        
-        
+        data["onsets"] = [x for x in (data["time"].jd -
+                                      data["time"].jd[0])/exptime*spacing]
+
     def _pyo_play(self):
         """
         TODO: document
@@ -122,14 +126,18 @@ class SoniSeries():
 
         # Getting data ready
         duration = self.data.meta["note_duration"]
-        pitches = np.repeat( self.data["pitch"], 2)
-        delays = np.repeat( self.data["onsets"], 2)
+        pitches = np.repeat(self.data["pitch"], 2)
+        delays = np.repeat(self.data["onsets"], 2)
 
-        # TODO: This doesn't seem like the best way to do this, but I don't know how to make it better
-        env = pyo.Linseg(list=[(0,0),(.01,1),(duration-.1,1),(duration-.05,0.5),(duration-.005,0)],
-                         mul=[.1 for i in range(len(pitches))]).play(delay=list(delays), dur=duration)
+        # TODO: This doesn't seem like the best way to do this, but I don't know
+        # how to make it better
+        env = pyo.Linseg(list=[(0, 0), (0.01, 1), (duration - 0.1, 1),
+                               (duration - 0.05, 0.5), (duration - 0.005, 0)],
+                         mul=[.1 for i in range(len(pitches))]).play(
+                             delay=list(delays), dur=duration)
 
-        self.streams = pyo.Sine(list(pitches), 0, env).out(delay=list(delays), dur=duration)
+        self.streams = pyo.Sine(list(pitches), 0, env).out(delay=list(delays),
+                                                           dur=duration)
 
     def _pyo_stop(self):
         """
@@ -145,8 +153,8 @@ class SoniSeries():
 
         # Getting data ready
         duration = self.data.meta["note_duration"]
-        pitches = np.repeat( self.data["pitch"], 2)
-        delays = np.repeat( self.data["onsets"], 2)
+        pitches = np.repeat(self.data["pitch"], 2)
+        delays = np.repeat(self.data["onsets"], 2)
 
         # Making sure we have a clean server
         if self.server.getIsBooted():
@@ -156,9 +164,12 @@ class SoniSeries():
         self.server.boot()
         self.server.recordOptions(dur=delays[-1]+duration, filename=filepath)
 
-        env = pyo.Linseg(list=[(0,0),(.1,1),(duration-.1,1),(duration-.05,0.5),(duration-.005,0)],
-                         mul=[.05 for i in range(len(pitches))]).play(delay=list(delays), dur=duration)
-        sine = pyo.Sine(list(pitches), 0, env).out(delay=list(delays), dur=duration)
+        env = pyo.Linseg(list=[(0, 0), (0.1, 1), (duration - 0.1, 1),
+                               (duration - 0.05, 0.5), (duration - 0.005, 0)],
+                         mul=[0.05 for i in range(len(pitches))]).play(
+                             delay=list(delays), dur=duration)
+        sine = pyo.Sine(list(pitches), 0, env).out(delay=list(delays),
+                                                   dur=duration)
         self.server.start()
 
         # Clean up
@@ -191,7 +202,6 @@ class SoniSeries():
             self._pyo_stop()
         else:  # thinkdsp
             self._thinkdsp_stop()
-        
 
     def write(self, filepath):
         """
@@ -201,9 +211,3 @@ class SoniSeries():
             self._pyo_write(filepath)
         else:  # thinkdsp
             self._thinkdsp_write(filepath)
-
-
-
-    
-    
-
