119fb83e5b9170c3610ee23904c2798707c845dc
Scott Fleming <fleming@stsci.edu>
2025-02-04 17:37:52 -0500

    fixing linting

--- astronify/series/series.py
+++ astronify/series/series.py
@@ -164,10 +164,14 @@ class SoniSeries:
         assert isinstance(data_table, Table), "Data must be a Table."
 
         if self.time_col not in data_table.columns:
-            raise AttributeError(f"Input Table must contain time column '{self.time_col}'")
+            raise AttributeError(
+                f"Input Table must contain time column '{self.time_col}'"
+            )
 
         if self.val_col not in data_table.columns:
-            raise AttributeError(f"Input Table must contain a value column '{self.val_col}'")
+            raise AttributeError(
+                f"Input Table must contain a value column '{self.val_col}'"
+            )
 
         # Removing any masked values as they interfere with the sonification
         if isinstance(data_table[self.val_col], MaskedColumn):
